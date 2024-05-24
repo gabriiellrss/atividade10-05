c73f221a506bef672b0cb0f29290be7488d34188
gabriiellrss <gabriel.santana166@etec.sp.gov.br>
2024-05-24 15:36:38 -0300

    escola

---
+++ biblioteca.h
@@ -0,0 +1,29 @@
+
+
+float soma(float n1, float n2) {
+	float resultado;
+	resultado = n1 + n2;
+	printf("resultado %f", resultado);
+	return resultado;
+}
+
+float subtrai(float n1, float n2) {
+	float resultado;
+	resultado = n1 - n2;
+	printf("resultado %f", resultado);
+	return resultado;
+}
+
+float multi(float n1, float n2) {
+	float resultado;
+	resultado = n1*n2;
+	printf("resultado %f", resultado);
+	return resultado;
+}
+
+float divisao(float n1, float n2) {
+	float resultado;
+	resultado = n1/n2;
+	printf("resultado %f", resultado);
+	return resultado;
+}
--- funcao-rep.c
+++ funcao-rep.c
@@ -1,21 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-
-void numero(int n) {
-	printf("%d\n", n);
-	n =n+1;
-	if (n==50){
-		numero(n);
-	} else {
-		printf("fim");
-	
-	}
-}
+#include"biblioteca.h"
 
 
 void main() {
-	printf("Boa Tarde\n");
-	numero(0);
+	float n1, n2, resultado;
+	int op;
+	op = 0;
 
+	while (op!=5) {
+	printf(" Operações: Escolha digitando um numero\n");
+	printf("\t 1.Soma\n");
+	printf("\t 2.Subtração\n");
+	printf("\t 3.Multiplicação\n");
+	printf("\t 4.Divisão\n");
+	printf("\t 5.Sair\n");
+	scanf("%f", &op);
+
+	printf("digite o primeiro valor\n");
+	scanf("%f", &n1);
+	printf("digite o segundo valor\n");
+	scanf("%f", &n2);
+	switch (op){
+		case 1:
+		resultado = soma(n1,n2);
+		break;
+
+		case 2:
+		resultado = subtrai(n1,n2);
+		break;
+
+		case 3:
+		resultado = multi(n1,n2);
+		break;
+
+		case 4:
+		if (n2==0) {
+			printf("ERRO! Digite um outro valor\n");
+			scanf("%f", &n2);
+			resultado = divisao(n1,n2);
+			break;
+		} else {
+			resultado = divisao(n1,n2);
+			break;
+		}}}
+
+
+		
 }
